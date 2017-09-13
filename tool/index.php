ee34d46af964cc0518518b23f0be5f93cab352d3
chokosuki4400 <e.bird.onishi@gmail.com>
2017-09-14 04:18:41 +0900

    php開発環境

---
+++ tool/index.php
@@ -0,0 +1,16 @@
+<?php
+$msg = "Hello, PHP!";
+?>
+ 
+ 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo $msg; ?></h1>
+</body>
+</html>
